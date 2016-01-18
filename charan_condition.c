71d493ecf9e63774eb8ada1a6de402f8a353491b
Saicharan <saicharan@localhost.localdomain>
2016-01-18 15:52:22 +0530

    Charan

--- charan_condition.c
+++ charan_condition.c
@@ -1,27 +1,44 @@
 #include<stdio.h>
 #include<pthread.h>
+int count=0;
+pthread_cond_t threshold;
+pthread_mutex_t lock;
 void *increment()
 {
-for(;;)
+int i;
+for(i=0;i<20;i++)
 {
 pthread_mutex_lock(&lock);
 count++;
 if (count==12)
 pthread_cond_signal(&threshold);
 pthread_mutex_unlock(&lock);
+pthread_exit(NULL);
 }
 }
 void *increment1()
 	{
-pthread_cond_wait(&threshold);
-
+pthread_mutex_lock(&lock);
+while (count <12)
+{
+pthread_cond_wait(&threshold,&lock);
+count+=10;
 }
-int count=0;
-pthread_cond_t threshold;
-pthread_mutex_t lock;
+pthread_mutex_unlock(&lock);
+pthread_exit(NULL);
+}
+
 int main()
 {
+pthread_attr_t join;
 pthread_t charan,varun;
-pthread_create(&charan,NULL,increment,NULL);
-pthread_create(&varun,NULL,increment1,NULL);
+pthread_attr_setdetachstate(&join,PTHREAD_CREATE_JOINABLE);
+pthread_create(&charan,&join,increment,NULL);
+pthread_create(&varun,&join,increment1,NULL);
+pthread_join(charan,NULL);
+pthread_join(varun,NULL);
+pthread_cond_destroy(&threshold);
+pthread_mutex_destroy(&lock);
+pthread_attr_destroy(&join);
+pthread_exit(NULL);
 }
